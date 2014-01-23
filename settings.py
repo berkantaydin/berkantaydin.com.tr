1476945b493e8afd49a6b3e0db2de73cb11d8b39
Berkant AYDIN <ba@berkantaydin.com.tr>
2014-01-23 19:19:23 +0200

    travis and pelican

---
+++ settings.py
@@ -0,0 +1,16 @@
+AUTHOR = "Berkant AYDIN"
+SITEURL = "http://berkantaydin.com.tr"
+SITENAME = "Berkant AYDIN"
+LOCALE = 'tr_TR.utf8'
+TIMEZONE = 'Europe/Istanbul'
+
+DISPLAY_PAGES_ON_MENU = True
+WITH_PAGINATION = True
+DEFAULT_PAGINATION = 10
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+RELATIVE_URLS = False
+DELETE_OUTPUT_DIRECTORY = True
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
